2dbb465d4018622e4f8929b3dc7c18b6227c953f
dhanikwp <dhanikusuma256@gmail.com>
2026-04-07 23:20:18 +0700

    push ulang entity employee ga ke save aduhhhhhhhhh nangis

--- src/entity/Employee.java
+++ src/entity/Employee.java
@@ -9,5 +9,24 @@ package entity;
  * @author macbook
  */
 public class Employee {
+    private String nik;
+    private String namaLengkap;
+    private String jabatan;
+    private String departemen;
+    private String statusKerja;
     
+    public Employee(String nik, String namaLengkap, String jabatan, String departemen, String statusKerja) {
+        this.nik = nik;
+        this.namaLengkap = namaLengkap;
+        this.jabatan = jabatan;
+        this.departemen = departemen;
+        this.statusKerja = statusKerja;
+    }
+    
+    public String getNik() { return nik; }
+    public String getNamaLengkap() { return namaLengkap; }
+    public String getJabatan() { return jabatan; }
+    public String getDepartemen() { return departemen; }
+    public String getStatusKerja() { return statusKerja; }
+
 }
